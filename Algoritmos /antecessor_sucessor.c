d5d2e2b5401fa458aee23a3349869af14ca4057e
Ph.almeida01 <ph.dev021@gmail.com>
2024-12-19 09:17:22 -0300

    Create antecessor_sucessor.c

---
+++ Algoritmos /antecessor_sucessor.c
@@ -0,0 +1,22 @@
+//O programa lê um número inteiro digitado pelo usuário e exibe seu antecessor e sucessor. Ideal para praticar operações matemáticas simples e entrada/saída de dados em C. 
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <locale.h>
+
+int main()
+{
+    setlocale(LC_ALL, "Portuguese");
+
+    int num, antecessor, sucessor;
+
+    printf("Digite um número: ");
+    scanf("%d", &num);
+
+    antecessor = num - 1;
+    sucessor = num + 1;
+
+    printf("O antecessor do número %d é o número %d e o sucessor é o número %d.\n", num, antecessor, sucessor);
+
+    return 0;
+}
