a68a9a034330362022335974f13a59e9fe881315
Ph.almeida01 <ph.dev021@gmail.com>
2024-12-19 22:15:48 -0300

    Create rotacionada_180_graus.c

---
+++ Matrizes /rotacionada_180_graus.c
@@ -0,0 +1,41 @@
+// Matriz 4x4 rotacionada em 180 graus
+
+#include <stdio.h>
+#include <stdlib.h>
+#define TAM 4
+
+int main() {
+    int mat[TAM][TAM];
+     int mat180[TAM][TAM];
+
+    printf("Preencha a matriz (%dx%d):\n", TAM, TAM);
+    for (int i = 0; i < TAM; i++) {
+        for (int j = 0; j < TAM; j++) {
+            scanf("%d", &mat[i][j]);
+        }
+    }
+
+    printf("\nMatriz Original Preenchida:\n");
+    for (int i = 0; i < TAM; i++) {
+        for (int j = 0; j < TAM; j++) {
+            printf("%d ", mat[i][j]);
+        }
+        printf("\n");
+    }
+
+    for (int i = 0; i < TAM; i++) {
+        for (int j = 0; j < TAM; j++) {
+            mat180[i][j] = mat[TAM - 1 - i][TAM - 1 - j];
+        }
+    }
+    printf("\nMatriz Rotacionada 180 Graus:\n");
+    for (int i = 0; i < TAM; i++) {
+        for (int j = 0; j < TAM; j++) {
+            printf("%d ", mat180[i][j]);
+        }
+        printf("\n");
+    }
+
+
+       return 0;
+}
